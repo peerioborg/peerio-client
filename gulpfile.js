7665ade4848030c8b3772215e19075a81425bc4e
Anri Asaturov <anri82@gmail.com>
2016-07-30 19:14:49 +0300

    chore(build): disable buggy chromium auto-install

--- gulpfile.js
+++ gulpfile.js
@@ -196,6 +196,7 @@ gulp.task('pack-mac-build', function(callback){
         basepath: __dirname,
         specification: {
             title: "Peerio",
+            format: "UDRW",
             icon:"./application/img/nw.icns",
             background:"./application/img/dmgBackground.png",
             contents: [
